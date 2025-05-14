cba89da484eb7a34049e8704a6f3b6d9eac20828
sipsatisa <siphiwe.shipalane@atisasoftware.co.za>
2025-05-14 13:43:04 +0200

    feat:Savechanges

---
+++ CFWebAPI/API/externalregistration.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CFWebAPI.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class externalregistration : ControllerBase
+    {
+    }
+}
