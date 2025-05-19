4c87f94375ed9414000e5eca37ba822aea41c480
sipsatisa <siphiwe.shipalane@atisasoftware.co.za>
2025-05-19 15:26:16 +0200

    feat:adding swaggerUI

--- CFWebAPI/Program.cs
+++ CFWebAPI/Program.cs
@@ -10,11 +10,12 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "CFWebAPI V1");
+    // c.RoutePrefix = "";    // uncomment if you want the UI at https://localhost:5000/
+});
 
 app.UseHttpsRedirection();
 
